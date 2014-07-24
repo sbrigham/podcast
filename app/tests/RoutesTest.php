decbaf859e44f5511802f685c5a3ce79ccbe67a1
Spencer Brigham <=>
2014-07-23 23:32:53 -0400

    Add basic route tests

--- app/tests/RoutesTest.php
+++ app/tests/RoutesTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Brigham\Podcast\Repositories\EloquentPodcastRepository;
-use Brigham\Podcast\Services\PodcastUpdater;
 
 class RoutesTest extends TestCase {
     public function test_home_route()
@@ -11,9 +9,22 @@ class RoutesTest extends TestCase {
         $this->assertResponseOk();
     }
 
-    public function test_show_routes()
+    public function test_show_all_route()
     {
+        $this->call('GET', '/shows');
+        $this->assertResponseOk();
+    }
 
+    public function test_show_episodes_route()
+    {
+        Route::enableFilters();
+        $this->call('GET', '/1');
+        $this->assertResponseOk();
+    }
+
+    public function test_episode_about_route()
+    {
+        $this->call('GET', '/1/episode/1');
     }
 }
  
