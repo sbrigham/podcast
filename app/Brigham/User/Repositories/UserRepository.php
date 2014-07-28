22e6fb36474379febe60772d559628aff4f3a0ba
Spencer Brigham <=>
2014-07-27 21:32:14 -0400

    Hash password

--- app/Brigham/User/Repositories/UserRepository.php
+++ app/Brigham/User/Repositories/UserRepository.php
@@ -2,10 +2,12 @@
 
 namespace Brigham\User\Repositories;
 
+use Illuminate\Support\Facades\Hash;
 use User;
 
 class UserRepository implements UserRepositoryInterface {
     public function create($user) {
+        $user['password'] = Hash::make($user['password']);
         return User::create($user);
     }
 }
