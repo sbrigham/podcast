edf2532f98d0e735fe94a617af54aad87826d53c
Spencer Brigham <=>
2014-08-25 22:34:47 -0400

    Fix #7. Add $http cache instead

--- public/js/Podcast/services/podcastService.js
+++ public/js/Podcast/services/podcastService.js
@@ -3,16 +3,16 @@ angular.module('podcastService', [])
     .factory('Podcast', function($http) {
         return {
             getShow: function(show_id) {
-                return $http.get('/api/show/' + show_id);
+                return $http.get('/api/show/' + show_id, { cache: true});
             },
             getShows : function() {
-                return $http.get('/api/shows');
+                return $http.get('/api/shows', { cache: true});
             },
             getEpisodes: function (show_id) {
-                return $http.get('/api/show/' + show_id + '/episodes');
+                return $http.get('/api/show/' + show_id + '/episodes', { cache: true});
             },
             getEpisode: function (episode_id) {
-                return $http.get('/api/episode/' + episode_id);
+                return $http.get('/api/episode/' + episode_id, { cache: true});
             }
         }
 
